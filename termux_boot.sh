af811f089dec6a1d63bda00a65a177d272a7f06a
bncr1234 <kenth1977@gmail.com>
2026-07-31 11:04:56 -0600

    Arregla scripts .sh con LF, .gitattributes y guia de failover

--- termux_boot.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-# termux_boot.sh - Arranque automatico de La Tribu en Android
-# Copiar a: ~/.termux/boot/start_latribu.sh
-
-termux-wake-lock
-sleep 15
-cd ~/plantillaFlask2026
-./reiniciar_tribu.sh
