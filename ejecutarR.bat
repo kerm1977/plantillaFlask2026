cdd4cd78dd4116f3d0fd05a8afe8d5b23a46bf18
bncr1234 <kenth1977@gmail.com>
2026-08-11 11:05:47 -0600

    Add JSON backup for cotizaciones

--- ejecutarR.bat
+++ ejecutarR.bat
@@ -1,147 +1,74 @@
-:: ejecutar_tribu.bat
-@echo off
-:: ===================================================
-:: CORRECCION DE RUTA (Obliga a la consola a quedarse aqui)
-:: ===================================================
-cd /d "%~dp0"
 
-:: ===================================================
-:: ENRUTADOR DE PROCESOS - LA TRIBU (Puerto 5050)
-:: ===================================================
-if "%~1"=="TRIBU_WATCHER" goto tribu_watcher
-if "%~1"=="TAIL_WATCHER" goto tail_watcher
-if "%~1"=="CLOUD_WATCHER" goto cloud_watcher
+[PWA] SW registrado: http://127.0.0.1:5050/ pwa_service.js:9:34
+Uncaught (in promise) SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data 2
+Error al interpretar el valor para '-webkit-text-size-adjust'.  Declaración rechazada. bootstrap.css:205:29
+Pseudo-clase o pseudo-elemento desconocido '-moz-focus-inner'.  Juego de reglas ignoradas debido a un mal selector. bootstrap.css:499:2
+Pseudo-clase o pseudo-elemento desconocido '-moz-focus-outer'.  Juego de reglas ignoradas debido a un mal selector. bootstrap.css:2523:13
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8333:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8338:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8343:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8348:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8353:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:8358:19
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9721:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9725:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9729:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9733:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9737:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:9741:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10282:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10286:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10290:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10294:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10298:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10302:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10843:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10847:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10851:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10855:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10859:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:10863:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11404:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11408:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11412:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11416:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11420:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11424:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11965:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11969:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11973:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11977:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11981:21
+Propiedad desconocida '-moz-column-gap'.  Declaración rechazada. bootstrap.css:11985:21
+Propiedad desconocida '-moz-osx-font-smoothing'.  Declaración rechazada. bootstrap-icons.css:26:27
+Error en el mapeo fuente: Error: request failed with status 404
+Stack in the worker:networkRequest@resource://devtools/client/shared/source-map-loader/utils/network-request.js:43:9
 
-:: ===================================================
-:: MENU PRINCIPAL INTERACTIVO
-:: ===================================================
-title Lanzador Maestro (La Tribu)
-color 0B
+URL del recurso: http://127.0.0.1:5050/static/css/bootstrap.css?v=2
+URL del mapa fuente: bootstrap.css.map
+XHR 
+POST
+http://127.0.0.1:5050/cotizadores/crear
+[HTTP/1.1 500 INTERNAL SERVER ERROR 92ms]
+XHR 
+POST
+http://127.0.0.1:5050/cotizadores/crear
+[HTTP/1.1 500 INTERNAL SERVER ERROR 62ms]
+Uncaught (in promise) SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data 2
+XHR 
+POST
+http://127.0.0.1:5050/cotizadores/crear
+[HTTP/1.1 500 INTERNAL SERVER ERROR 118ms]
+XHR 
+POST
+http://127.0.0.1:5050/cotizadores/crear
+[HTTP/1.1 500 INTERNAL SERVER ERROR 66ms]
+Uncaught (in promise) SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data 2
+XHR 
+POST
+http://127.0.0.1:5050/cotizadores/crear
+[HTTP/1.1 500 INTERNAL SERVER ERROR 85ms]
+Uncaught (in promise) SyntaxError: JSON.parse: unexpected character at line 1 column 1 of the JSON data 2
 
-echo =======================================================
-echo     SISTEMA DE ENRUTAMIENTO: CAMINATAS LA TRIBU
-echo =======================================================
-echo.
-echo Seleccione el modo de conexion que desea iniciar hoy:
-echo.
-echo   [1] Modo Cloudflare (Solo Dominio)
-echo       - Expone tu app en latribu.top
-echo       - Abre una ventana visible del tunel.
-echo.
-echo   [2] Modo Tailscale Funnel (Solo Subdominio .ts.net)
-echo       - Expone tu app usando el tunel publico de Tailscale.
-echo.
-echo   [3] Modo Hibrido Total (Ambos)
-echo       - Activa latribu.top Y tu dominio de Tailscale a la vez.
-echo.
-echo   [4] LIMPIEZA: Desinstalar servicio invisible (Solo usar 1 vez)
-echo       - Borra el servicio de fondo que daba el error 1033.
-echo.
-set /p opcion="Elija una opcion (1, 2, 3 o 4): "
+​
 
-if "%opcion%"=="4" goto limpiar_cloudflare
-
-echo.
-echo [*] Preparando el entorno...
-echo [*] Liberando puerto local 5050 para evitar choques...
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :5050 ^| findstr LISTENING') do taskkill /F /PID %%a > nul 2>&1
-
-echo [*] Iniciando App de La Tribu (Servidor Flask)...
-start "TRIBU_WATCHER" cmd /c "%~f0" TRIBU_WATCHER
-
-if "%opcion%"=="1" goto iniciar_cloudflare
-if "%opcion%"=="2" goto iniciar_tailscale
-if "%opcion%"=="3" goto iniciar_ambos
-
-:iniciar_cloudflare
-echo [*] Iniciando Tunel de Cloudflare (latribu.top)...
-start "CLOUD_WATCHER" cmd /c "%~f0" CLOUD_WATCHER
-goto fin
-
-:iniciar_tailscale
-echo [*] Iniciando Enrutador Tailscale Funnel...
-start "TAIL_WATCHER" cmd /c "%~f0" TAIL_WATCHER
-goto fin
-
-:iniciar_ambos
-echo [*] Iniciando Tunel de Cloudflare (latribu.top)...
-start "CLOUD_WATCHER" cmd /c "%~f0" CLOUD_WATCHER
-echo [*] Iniciando Enrutador Tailscale Funnel...
-start "TAIL_WATCHER" cmd /c "%~f0" TAIL_WATCHER
-goto fin
-
-:limpiar_cloudflare
-echo.
-echo =======================================================
-echo   LIMPIANDO EL SERVICIO INVISIBLE DE CLOUDFLARE
-echo =======================================================
-echo ATENCION: Debiste abrir este archivo como Administrador.
-echo.
-cloudflared.exe service uninstall
-echo.
-echo [OK] Limpieza terminada. 
-echo Cierra esta ventana y vuelve a abrir el archivo normalmente (Opcion 1).
-pause
-exit
-
-:fin
-echo.
-echo =======================================================
-echo   TODO LISTO. Puede minimizar o cerrar esta ventana.
-echo   Veras nuevas ventanas con los servidores corriendo.
-echo =======================================================
-pause
-exit
-
-:: ===================================================
-:: SUB-PROCESO: FLASK APP
-:: ===================================================
-:tribu_watcher
-cd /d "%~dp0"
-title TRIBU_APP (Local: 5050)
-color 0A
-set PY_CMD=python
-if exist "env\Scripts\python.exe" set PY_CMD=env\Scripts\python.exe
-
-:tribu_loop
-echo [!] Corriendo La Tribu con: %PY_CMD%
-set PORT=5050
-%PY_CMD% app.py
-echo [X] Caida en La Tribu. Reiniciando en 3 segundos...
-timeout /t 3 > nul
-goto tribu_loop
-
-:: ===================================================
-:: SUB-PROCESO: TAILSCALE FUNNEL
-:: ===================================================
-:tail_watcher
-cd /d "%~dp0"
-title TAILSCALE_MANAGER_TRIBU
-color 0D
-echo ===================================================
-echo [!] CONFIGURANDO ENRUTAMIENTO TAILSCALE
-echo ===================================================
-echo.
-echo [*] Creando ruta publica para LA TRIBU (Puerto 8443)...
-tailscale funnel --bg --https=8443 http://127.0.0.1:5050
-timeout /t 3 > nul
-echo.
-echo [OK] Tunnel de Tailscale creado correctamente.
-exit
-
-:: ===================================================
-:: SUB-PROCESO: CLOUDFLARE TUNNEL
-:: ===================================================
-:cloud_watcher
-cd /d "%~dp0"
-title CLOUDFLARE_TUNNEL_TRIBU
-color 0E
-echo ===================================================
-echo [!] CONECTANDO CON CLOUDFLARE (latribu.top)
-echo ===================================================
-echo.
-cloudflared.exe tunnel run --token eyJhIjoiZmZhMDJmYjFkYjUwMzBhMGYzMjBlYjAxMTIxYzJjZmEiLCJ0IjoiZTY3YjVhYWQtMTVjZi00N2M4LTk3YzctZjQzNTlhMDIwNzExIiwicyI6Ik56WmlNRFpqWW1NdFkyWTRZaTAwTVdNMExXRXpPRE10WVdFeVltVTNZbUpoTkRjdyJ9
-echo.
-echo [X] El tunel se cerro inesperadamente.
-pause
-exit
