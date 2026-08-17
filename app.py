1e1f2aa9c82328f24876b1a09f9e898c81744c30
bncr1234 <kenth1977@gmail.com>
2026-08-17 15:14:01 -0600

    Desactivar reloader de Flask para Termux

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     port = int(os.environ.get('PORT', 5050))
-    socketio.run(app, debug=Config.DEBUG, host='0.0.0.0', port=port)
+    socketio.run(app, debug=Config.DEBUG, host='0.0.0.0', port=port, use_reloader=False)
