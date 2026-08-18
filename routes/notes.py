25a2f2f0d25148afb1c20f1faff724014adfeadc
bncr1234 <kenth1977@gmail.com>
2026-08-18 11:07:58 -0600

    Registrar rutas de notas

--- routes/notes.py
+++ routes/notes.py
@@ -1,1 +1,9 @@
-from . import notes_admin, notes_public, notes_import_export, notes_socketio
+from . import notes_admin
+from routes import bp
+
+bp.add_url_rule('/api/notes', view_func=notes_admin.list_notes, methods=['GET'])
+bp.add_url_rule('/api/notes', view_func=notes_admin.create_note, methods=['POST'])
+bp.add_url_rule('/api/notes/<int:note_id>', view_func=notes_admin.update_note, methods=['PUT'])
+bp.add_url_rule('/api/notes/<int:note_id>', view_func=notes_admin.delete_note, methods=['DELETE'])
+bp.add_url_rule('/api/notes/<int:note_id>/share', view_func=notes_admin.share_note, methods=['POST'])
+bp.add_url_rule('/api/notes/<int:note_id>/unshare', view_func=notes_admin.unshare_note, methods=['POST'])
